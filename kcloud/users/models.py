be0dacc302465d77c2a689f9b899694904f764bd
Kostya <run06mail.ru@gmail.com>
2024-11-13 00:52:36 +0300

    Created user model

---
+++ kcloud/users/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    subscription_plan = models.CharField(
+        max_length=20,
+        choices=[('free', 'Free'), ('basic', 'Basic'), ('premium', 'Premium')],
+        default='free'
+    )
+
+    storage_limit = models.BigIntegerField(default=5*1024*1024*1024)
+    storage_used = models.BigIntegerField(default=0)
+
+    is_email_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_ip_user = models.GenericIPAddressField(null=True, blank=True)
+
+    two_factor_auth_enabled = models.BooleanField(default=False)
+    security_question = models.CharField(max_length=255, null=True, blank=True)
+    security_answer_hash = models.CharField(max_length=255, null=True, blank=True)  # Хеш ответа для безопасности
+
+    # Метаданные
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f"{self.email}"
+
+    def update_limit(self, file_size):
+        self.storage_used += file_size
+
+        if self.storage_used > self.storage_limit:
+            raise ValueError('Превышен лимит хранения')
+
+        self.save()
